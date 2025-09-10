25b6a23c01df19218f354834a7804bd28a98724d
Manivannan <yuvanmani007@gmail.com>
2025-09-10 17:55:43 +0530

    back end URL changed, important note updated in readme.md file

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/api/v1";
+const baseURL = "https://notes-backend-f3bj.onrender.com/api/v1";
 
 const instance = axios.create({
     baseURL,
